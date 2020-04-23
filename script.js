71ab076c03c4b46935b0511bc4bf1c5902c0b2cd
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-23 16:29:10 -0300

    requisitos 1 a 6

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let btnlogin = document.getElementById('button-login');
+btnlogin.addEventListener('click', function () {
+  let nome = document.getElementById('user-email-phone').value;
+  alert(nome);
+});
