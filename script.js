32c8e52ca2c8eb7615dae0175b5f101ba41e2d5a
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-23 18:27:10 -0300

    datepicker and radio buttons

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const btnLogin = document.querySelector('#button-login');
 const user = document.querySelector('#user-email-phone');
 const password = document.querySelector('#senha');
+const slctDia = document.querySelector('#dia');
+const slctMes = document.querySelector('#mes');
+const slctAno = document.querySelector('#ano');
 
-btnLogin.addEventListener('click', function () {
-  alert(`User: ${user.value} password: ${password.value}`);
-});
+window.onload = function () {
+  document.getElementById('data').DatePickerX.init( 
+  );
+
+  btnLogin.addEventListener('click', function () {
+    alert(`User: ${user.value} password: ${password.value}`);
+  });
+}
