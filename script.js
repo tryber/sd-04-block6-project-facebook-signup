b7632df0cd1daa56caccf46978572ac3e93e00b3
joaosoutto <joaosoutto92@gmail.com>
2020-04-23 18:10:56 -0300

    tentando requisitos

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const email = document.getElementById('user-email-phone');
 const login = document.getElementById('button-login');
 
 
-//  mostrar email ao clicar no botão
+//  REQUISITO 7 mostrar email ao clicar no botão
 login.addEventListener('click', function showEmail() {
   alert(email.value);
 });
