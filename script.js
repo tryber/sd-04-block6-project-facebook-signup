c88e4d63898d4997231530f350adcfa91f7af5c5
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-23 19:27:13 -0300

    until 11th step 2nd try

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const entryButton = document.getElementById('button-login');
 const emailInput = document.getElementById('user-email-phone');
-const passInput = document.getElementById('user-password');
 
 entryButton.addEventListener('click', function () {
   alert(emailInput.value);
