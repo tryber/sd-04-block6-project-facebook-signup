fa986655dd8663ca5e6c6c5b593db6be66df590a
Julianafatsil <julianafatsil@gmail.com>
2020-04-23 18:08:35 -0300

    Creation of the facebook login page

---
+++ scripty.js
@@ -0,0 +1,12 @@
+
+const BTNENVIAR = document.getElementById('button-login');
+
+function BtnEntrar() {
+    alert('Você clicou em Entrar');
+}
+
+BTNENVIAR.addEventListener('click', function () {
+  BtnEntrar();
+  });
+
+
