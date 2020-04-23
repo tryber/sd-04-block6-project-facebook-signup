6dc4e103c7446b74b9b80033d08a26650057ee3b
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-23 19:40:22 -0300

    html, js, css - Requisitos 7 a 15

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.onload = function () {
+let btnLogin;
+let user;
 
+window.onload = function () {
+  btnLogin = document.getElementById('button-login');
+  user = document.getElementById('user-email-phone');
+
+  btnLogin.addEventListener('click', function () {
+    alert(user.value);
+  });
 };
