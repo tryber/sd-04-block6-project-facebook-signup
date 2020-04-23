ca1dd9faeef07b21b1daf71041d3676d05352cdf
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-23 18:48:46 -0300

    solving issues

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const btnLogin = document.querySelector('#button-login');
 const user = document.querySelector('#user-email-phone');
-const password = document.querySelector('#senha');
+const password = document.querySelector('#user-password');
 const slctDia = document.querySelector('#dia');
 const slctMes = document.querySelector('#mes');
 const slctAno = document.querySelector('#ano');
 
 window.onload = function () {
-  document.getElementById('data').DatePickerX.init( 
-  );
+  document.getElementById('data').DatePickerX.init();
 
   btnLogin.addEventListener('click', function () {
     alert(`User: ${user.value} password: ${password.value}`);
