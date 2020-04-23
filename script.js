5be823262604f81d8ef7aea8646a007e950d0a00
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-23 18:55:31 -0300

    adding signin button

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const btnLogin = document.querySelector('#button-login');
 const user = document.querySelector('#user-email-phone');
 const password = document.querySelector('#user-password');
-const slctDia = document.querySelector('#dia');
-const slctMes = document.querySelector('#mes');
-const slctAno = document.querySelector('#ano');
 
 window.onload = function () {
-  document.getElementById('data').DatePickerX.init();
 
   btnLogin.addEventListener('click', function () {
     alert(`User: ${user.value} password: ${password.value}`);
   });
-}
+};
