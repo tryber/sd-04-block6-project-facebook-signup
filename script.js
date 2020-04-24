1c3ca57e9788b7470c610ae437995f2abcc80f6d
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-24 12:57:46 -0300

    moved font awesome library

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const loginButton = document.getElementById('button-login')
+
 const loginEmail = document.getElementById('user-email-phone')
-const loginPassword = document.getElementById('user-password')
 const loginForm = document.getElementById('fb-login-form')
 
 loginForm.addEventListener('submit', function(event){
   event.preventDefault()
   alert(loginEmail.value)
 })
-
