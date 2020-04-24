3905b6043c3d98b7d7712c896d119b7c3798b6b2
orlando-messias <programadorlando@gmail.com>
2020-04-24 13:50:37 -0300

    code climate issues

--- script.js
+++ script.js
@@ -5,29 +5,11 @@ const pass = document.getElementById('user-password');
 const register = document.getElementById('facebook-register');
 const formRegister = document.getElementById('form-register');
 
-btnLogin.addEventListener('click', () => {
-  event.preventDefault();
-  if (email.value || pass.value) {
-    alert(`${email.value}`);
-  }
-});
-
-register.addEventListener('click', () => {
-  event.preventDefault();
-  let genre = document.querySelector('[name=genre]:checked');
-  if (validate()) {
-    alert(`${username.value} - ${genre.value}`);
-  }
-  else {
-    alert('Dados Inválidos');
-  }
-})
-
 function validate() {
   let valid = true;
   let genreChecked = false;
-  [...formRegister.elements].forEach(field => {
-    if (['text','password'].indexOf(field.type) > -1 && field.value === '') {
+  [...formRegister.elements].forEach((field) => {
+    if (['text', 'password'].indexOf(field.type) > -1 && field.value === '') {
       valid = false;
     }
     if (field.name === 'genre' && field.checked) {
@@ -37,4 +19,23 @@ function validate() {
   if (valid && genreChecked) {
     return true;
   }
+  return false;
 }
+
+btnLogin.addEventListener('click', () => {
+  event.preventDefault();
+  if (email.value || pass.value) {
+    alert(`${email.value}`);
+  }
+});
+
+register.addEventListener('click', () => {
+  event.preventDefault();
+  const genre = document.querySelector('[name=genre]:checked');
+  if (validate()) {
+    alert(`${username.value} - ${genre.value}`);
+  }
+  else {
+    alert('Dados Inválidos');
+  }
+});
