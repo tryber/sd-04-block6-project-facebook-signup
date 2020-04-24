73d0b5d35b040be8cfa26ba95d4be97b3272bfe9
orlando-messias <programadorlando@gmail.com>
2020-04-24 11:00:13 -0300

    sccript alert shows email and password

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnLogin = document.getElementById('button-login');
+const email = document.getElementById('user-email-phone');
+const pass = document.getElementById('user-password');
+
+btnLogin.addEventListener('click', () => {
+    event.preventDefault();
+    if (email.value || pass.value) {
+        alert(`Confira as Informações:
+        Email: ${email.value}
+        Senha: ${pass.value}`);
+    }
+});
