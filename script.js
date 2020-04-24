c8516e0b2b9dffff5bbae95430de62df5d0321fd
orlando-messias <programadorlando@gmail.com>
2020-04-24 11:49:57 -0300

    alert

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const pass = document.getElementById('user-password');
 btnLogin.addEventListener('click', () => {
   event.preventDefault();
   if (email.value || pass.value) {
-    alert(`Confira seu E-mail ou Telefone: ${email.value}`);
+    alert(`${email.value}`);
   }
 });
