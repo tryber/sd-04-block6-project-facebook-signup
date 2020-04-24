8cf0032c7d33c368c4a05889963803183efdce54
orlando-messias <programadorlando@gmail.com>
2020-04-24 13:43:16 -0300

    form register validate

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btnLogin = document.getElementById('button-login');
+const username = document.getElementById('name');
 const email = document.getElementById('user-email-phone');
 const pass = document.getElementById('user-password');
+const register = document.getElementById('facebook-register');
+const formRegister = document.getElementById('form-register');
 
 btnLogin.addEventListener('click', () => {
   event.preventDefault();
@@ -8,3 +11,30 @@ btnLogin.addEventListener('click', () => {
     alert(`${email.value}`);
   }
 });
+
+register.addEventListener('click', () => {
+  event.preventDefault();
+  let genre = document.querySelector('[name=genre]:checked');
+  if (validate()) {
+    alert(`${username.value} - ${genre.value}`);
+  }
+  else {
+    alert('Dados Inválidos');
+  }
+})
+
+function validate() {
+  let valid = true;
+  let genreChecked = false;
+  [...formRegister.elements].forEach(field => {
+    if (['text','password'].indexOf(field.type) > -1 && field.value === '') {
+      valid = false;
+    }
+    if (field.name === 'genre' && field.checked) {
+      genreChecked = true;
+    }
+  });
+  if (valid && genreChecked) {
+    return true;
+  }
+}
