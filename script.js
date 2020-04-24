7cf7b2bc76af3f2595d890d3e25280ca5d3828dd
danilossousa <danilosantossousa@gmail.com>
2020-04-24 03:19:17 -0300

    v0.17.0.1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inpEmailTel = document.getElementById('user-email-phone');
 const butLogin = document.getElementById('button-login');
+const picker = new Pikaday({ field: document.getElementById('datepicker') });
 
 butLogin.addEventListener('click', function () {
   alert(inpEmailTel.value);
