86ced424e6877080bbdba01f81bca17ff4132dab
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-23 21:12:28 -0300

    html, js-Requisitos 16 a 20

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 let btnLogin;
+let btnRegister;
 let user;
+
+let name;
+let lastName;
+let phoneEmail;
+let newPassword;
+let birthday;
+let gender;
 
 window.onload = function () {
   btnLogin = document.getElementById('button-login');
+  btnRegister = document.getElementById('facebook-register');
   user = document.getElementById('user-email-phone');
+
+  name = document.getElementById('name');
+  lastName = document.getElementById('last-name');
+  phoneEmail = document.getElementById('phone-email');
+  newPassword = document.getElementById('new-password');
+  birthday = document.getElementById('birthday');
+  gender = document.getElementsByName('gender');
 
   btnLogin.addEventListener('click', function () {
     alert(user.value);
+  });
+
+  btnRegister.addEventListener('click', function () {
+    alert(name.value);
+    alert(gender[0].value);
   });
 };
