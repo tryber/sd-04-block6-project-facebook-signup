0251ba0c010fbf06005a3c8ebd242a4eaae6a5fe
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-25 19:14:04 -0300

    Adicionado evento para o botão entrar.

---
+++ script.js
@@ -0,0 +1,10 @@
+const btLogar = document.getElementById('button-login');
+const email = document.getElementById('user-email-phone');
+
+function exibeLogin() {
+    alert(email.value);
+}
+
+window.onload = function () {
+    btLogar.onclick = exibeLogin;
+}
