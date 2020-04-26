1038f8739a0573a80bea73498c23fb138adc1c7f
Thabata <dornelasthabata@gmail.com>
2020-04-26 16:08:18 -0300

    Ajustando espaços do style e script para Code Climate

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function cadastro() {
-    const nome=document.getElementById('nome').value;
-    const sobrenome=document.getElementById('sobrenome').value;
-    const celMail=document.getElementById('celularMail').value;
-    const senha=document.getElementById('senha').value;
-    const data=document.getElementById('data').value;
-if (nome.length && sobrenome.length && celMail.length && senha.length && data.length) {
-    window.alert(`${nome}${sobrenome}-${celMail}-${data}`);}
-else {
-    window.alert('Dados inválidos.');
-    }
+  const nome = document.getElementById('nome').value;
+  const sobrenome = document.getElementById('sobrenome').value;
+  const celMail = document.getElementById('celularMail').value;
+  const senha = document.getElementById('senha').value;
+  const data = document.getElementById('data').value;
+  if (nome.length && sobrenome.length && celMail.length && senha.length && data.length) {
+  window.alert(`${nome}${sobrenome}-${celMail}-${data}`);
+}
+  else {
+  window.alert('Dados inválidos.');
+}
 }
 
 function LOADED() {
-    const submitForms = document.getElementById('facebook-register');
-    submitForms.addEventListener('click', cadastro);
+  const submitForms = document.getElementById('facebook-register');
+  submitForms.addEventListener('click', cadastro);
 }
 
 window.onload = LOADED();
