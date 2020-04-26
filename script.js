ee8a933b7c5105c878083fa1f3b28c88b9b29dd4
Thabata <dornelasthabata@gmail.com>
2020-04-26 17:40:06 -0300

    Modificando nome do ID para passar no resquisito e tentando arrumar o button de cadastro

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function cadastro() {
   const nome = document.getElementById('nome').value;
   const sobrenome = document.getElementById('sobrenome').value;
   const celMail = document.getElementById('celularMail').value;
-  const senha = document.getElementById('senha').value;
+  const senha = document.getElementById('user-password').value;
   const data = document.getElementById('data').value;
   if (nome.length && sobrenome.length && celMail.length && senha.length && data.length) {
     window.alert(`${nome}${sobrenome}-${celMail}-${data}`);
