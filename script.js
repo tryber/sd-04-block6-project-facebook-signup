3b50c856801a278c789d2647d4104488abbe37cf
Thabata <dornelasthabata@gmail.com>
2020-04-27 20:53:12 -0300

    Reestruturando o JS

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-function cadastro() {
-  const nome = document.getElementById('nome').value;
-  const sobrenome = document.getElementById('sobrenome').value;
-  const celMail = document.getElementById('celularMail').value;
-  const senha = document.getElementById('user-password').value;
-  const data = document.getElementById('data').value;
-  if (nome.length && sobrenome.length && celMail.length && senha.length && data.length) {
-    window.alert(`${nome}${sobrenome}-${celMail}-${data}`);
-  } else {
-    window.alert('Dados inválidos.');
+const btnEntrar = document.getElementById('button-login');
+const emailInput = document.getElementById('user-email-phone');
+
+function AlertEmailPhone() {
+  if (emailInput.value !== '') {
+    alert(`${emailInput.value}`);
+} else {
+    alert('Email ou Telefone inválidos');
   }
 }
 
-function LOADED() {
-  const submitForms = document.getElementById('facebook-register');
-  submitForms.addEventListener('click', cadastro);
-}
-
-window.onload = LOADED();
+btnEntrar.addEventListener('click', AlertEmailPhone);
