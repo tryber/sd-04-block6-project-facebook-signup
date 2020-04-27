660cf7ddb7269f0bb5eeb1e2c2f21f8cc97030f7
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-27 13:19:45 -0300

    First Attempt

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnlogin = document.getElementById('button-login');
+btnlogin.addEventListener('click', function () {
+  const nome = document.getElementById('user-email-phone').value;
+  alert(nome);
+});
