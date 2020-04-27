6d5c35a779fd4d683ca3a473554886188b9d625f
danilossousa <danilosantossousa@gmail.com>
2020-04-27 09:27:32 -0300

    v0.20.3.4

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+let i;
 const inpEmailTel = document.getElementById('user-email-phone');
 const butLogin = document.getElementById('button-login');
+const form = document.querySelector('form');
+const radio = document.querySelectorAll('input[type="radio"]');
+
+console.log(radio.length)
 
 butLogin.addEventListener('click', function () {
   alert(inpEmailTel.value);
 });
 
-// nome sobrenome celemail senha datanascimento gênero
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  
+  const name = form[0].value;
+  const lastName = form[1].value;
+  const celEmail = form[2].value;
+  const date = form[4].value;
+  let radioCheck;
+
+  for (i = 0; i < radio.length; i += 1) {
+    if (radio[i].checked === true) {
+      radioCheck = radio[i].value;
+    }
+  }
+
+  alert(`${name} ${lastName} - ${celEmail} - ${date} - ${radioCheck}`);
+});
