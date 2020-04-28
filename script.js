82214e1122c3ae920699b5718bb381b079e9541c
Julianafatsil <julianafatsil@gmail.com>
2020-04-28 12:04:55 -0300

    correction of functions for items

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 const BTNENVIAR = document.getElementById('button-login');
 const USUARIO = document.getElementById('user-email-phone');
-const BTNVALIDAR = document.getElementById('facebook-register');
 
 function BtnEntrar() {
   alert(USUARIO.value);
