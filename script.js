7a75254fa13458f675659f856f2984813d5dabfe
Julianafatsil <julianafatsil@gmail.com>
2020-04-28 11:54:08 -0300

    correction of functions for item 20

--- script.js
+++ script.js
@@ -7,34 +7,6 @@ function BtnEntrar() {
   alert(USUARIO.value);
 }
 
-function GetGenre(genre) {
-  let result = '';
-  if (genre === '1') {
-    result = 'Feminino';
-  } else if (genre === '2') {
-    result = 'Masculino';
-  } else if (genre === '3') {
-    result = 'Personalizado';
-  }
-  return result;
-}
-
-function IsNotClear(text) {
-  let result = false;
-  if (text !== '') {
-    result = true;
-  }
-  return result;
-}
-
-function IsNotNull(text) {
-  let result = false;
-  if (text !== null) {
-    result = IsNotClear(text.value);
-  }
-  return result;
-}
-
 BTNENVIAR.addEventListener('click', function () {
   BtnEntrar();
 });
