cc227446337d7630ca7365340c890b4e8aa638c5
Caio Magalhães <caio.lmf@gmail.com>
2020-04-29 18:41:18 -0300

    Code cleaning

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const loginForm = document.querySelector('#login-form');
+const newUserForm = document.querySelector('#new-user-form');
+
 
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -6,3 +8,48 @@ loginForm.addEventListener('submit', (e) => {
 
   alert(userName);
 });
+
+newUserForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let newUserData = '';
+
+  const formElements = e.target.elements;
+  for (const element of formElements) {
+    if (
+      element.value === ''
+      && element.name !== 'gender'
+      && element.id !== 'facebook-register'
+    ) {
+      alert('Dados inválidos');
+
+      return;
+    }
+
+    if (!getRadioValue(e.target, 'gender')) {
+      alert('Dados inválidos');
+
+      return;
+    }
+
+    if (element.id !== 'facebook-register') {
+      if (element.id === 'name') {
+        newUserData += element.value;
+      } else if (element === getRadioValue(e.target, 'gender') || element.name !== 'gender') {
+        newUserData += ` - ${element.value}`;
+      }
+    }
+  }
+  alert(newUserData);
+});
+
+function getRadioValue(form, name) {
+  const radios = form.elements[name];
+  let selectedRadio = '';
+
+  for (const radio of radios) {
+    if (radio.checked) {
+      selectedRadio = radio;
+    }
+  }
+  return selectedRadio;
+}
