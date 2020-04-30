f2b77fecafd72e240a56b09088e1238cb9de3fb9
fogo5000 <lucaslucas2008@gmail.com>
2020-04-30 18:27:52 -0300

    Add reqisition 20 1.0 changes to CC 1

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const nome = document.getElementById("nome");
-const snome = document.getElementById("snome");
-const nome = document.getElementById("senha");
-const nome = document.getElementById("email");
-
-
+const nome = document.getElementById('nome');
+const snome = document.getElementById('snome');
+const nome = document.getElementById('senha');
+const nome = document.getElementById('email');
 
 function Iniciar() {
 
