26ae1b4a4fb0c7c9c5ef26caf3e97675eaae5941
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-04-30 13:06:17 -0300

    erros codeClimate css e js

--- script.js
+++ script.js
@@ -7,34 +7,34 @@ const gender = document.querySelectorAll('#sex');
 const button = document.getElementById('facebook-register');
 button.addEventListener('click', function () {
 if (nome.value === '') {
-	 alert('Campo Nome incorreto!');
- 	nome.focus();
+  alert('Campo Nome incorreto!');
+  nome.focus();
 }
 if (lastName.value === '') {
   alert('Campo Sobrenome incorreto!');
-	 lastName.focus();
+  lastName.focus();
 }
 if (phone.value === '') {
-	 alert('Campo Celular incorreto!');
-	 phone.focus();
+  alert('Campo Celular incorreto!');
+  phone.focus();
 }
 if (password.value === '') {
-		alert('Campo Senha incorreto!');
-		password.focus();
+  alert('Campo Senha incorreto!');
+  password.focus();
 }
 if (birth.value === '') {
-		alert('Campo Data de Nascimento incorreto!');
-		birth.focus();
+  alert('Campo Data de Nascimento incorreto!');
+  birth.focus();
 }
 for (let i = 0; i < gender.length; i += 1) {
-		let choice = this;
-		choice.addEventListener('click', function() {
-		if (choice === '') {
-		alert('Campo Gênero incorreto!');
-		gender.focus();
+  let choice = this;
+  choice.addEventListener('click', function() {
+if (choice === '') {
+  alert('Campo Gênero incorreto!');
+  gender.focus();
 }        
-  });
+});
 }
- const dados = `Nome: ${nome.value}, Sobrenome: ${lastName.value}, Celular/Email: ${phone.value}, Data de Nascimento: ${birth.value}, Gênero: ${gender.value}`;
- alert(dados);
+  const dados = `Nome: ${nome.value}, Sobrenome: ${lastName.value}, Celular/Email: ${phone.value}, Data de Nascimento: ${birth.value}, Gênero: ${gender.value}`;
+  alert(dados);
 });
